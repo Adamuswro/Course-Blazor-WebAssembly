c720a2643bbe5057fee739e013f756b70e0b3722
Adam Szulc <adszulc@danfoss.com>
2021-11-23 07:14:42 +0100

    Login form + validation

---
+++ BlazorBattles/Shared/UserLogin.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorBattles.Shared
+{
+    public class UserLogin
+    {
+        [Required(ErrorMessage ="Please, enter user name.")]
+        public string UserName { get; set; }
+        [Required(ErrorMessage = "Please, enter password.")]
+        public string Password { get; set; }
+    }
+}
