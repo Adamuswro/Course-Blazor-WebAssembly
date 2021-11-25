bef8b401578db653120f9864b21540a747b28556
Adam Szulc <adszulc@danfoss.com>
2021-11-25 10:20:36 +0100

    EntityFramework: Connection string added

--- BlazorBattles/Server/Startup.cs
+++ BlazorBattles/Server/Startup.cs
@@ -1,11 +1,10 @@
+using BlazorBattles.Server.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System.Linq;
 
 namespace BlazorBattles.Server
 {
@@ -22,7 +21,9 @@ namespace BlazorBattles.Server
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddDbContext<DataContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"))
+            );
             services.AddControllersWithViews();
             services.AddRazorPages();
         }
