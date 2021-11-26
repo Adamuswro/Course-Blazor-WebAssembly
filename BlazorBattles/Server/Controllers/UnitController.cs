bb84411639c3f8d5828ee6a16448b9a0b3a9badf
Adam Szulc <adszulc@danfoss.com>
2021-11-26 07:34:29 +0100

    CRUD for Unit finished

--- BlazorBattles/Server/Controllers/UnitController.cs
+++ BlazorBattles/Server/Controllers/UnitController.cs
@@ -1,11 +1,7 @@
 ﻿using BlazorBattles.Server.Data;
 using BlazorBattles.Shared;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace BlazorBattles.Server.Controllers
@@ -26,7 +22,49 @@ namespace BlazorBattles.Server.Controllers
         {
             var units = await context.Units.ToListAsync();
             return Ok(units);
-    }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddUnit(Unit unit)
+        {
+            context.Units.Add(unit);
+            await context.SaveChangesAsync();
+
+            return Ok(await context.Units.ToListAsync());
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateUnit(int id, Unit unit)
+        {
+            var dbUnit = await context.Units.FirstOrDefaultAsync(u => u.Id == id);
+            if (dbUnit == null)
+            {
+                return NotFound("Unit with the given Id doesn't exist.");
+            }
+
+            dbUnit.Title = unit.Title;
+            dbUnit.Defense = unit.Defense;
+            dbUnit.Attack = unit.Attack;
+            dbUnit.BananaCost = unit.BananaCost;
+            await context.SaveChangesAsync();
+
+            return Ok(dbUnit);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUnit(int id)
+        {
+            var dbUnit = await context.Units.FirstOrDefaultAsync(u => u.Id == id);
+            if (dbUnit == null)
+            {
+                return NotFound("Unit with the given Id doesn't exist.");
+            }
+
+            context.Remove(dbUnit);
+            await context.SaveChangesAsync();
+
+            return Ok(await context.Units.ToListAsync());
+        }
     }
 
 
